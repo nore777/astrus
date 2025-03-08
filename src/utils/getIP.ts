b3418f019f7953c31676f2890f20cf5a07f6c020
lukas <lukas.noreikis@gmail.com>
2025-03-08 19:49:11 +0200

    find the host machine's ip address

---
+++ src/utils/getIP.ts
@@ -0,0 +1,26 @@
+import { networkInterfaces } from "node:os"
+
+export default function getIP(): string | undefined {
+  const net = networkInterfaces()
+
+  let IP = null;
+
+  for (const interfaceName in net) {
+
+    const networkInterface = net[interfaceName]
+    if (!networkInterface) continue
+
+    for (const i of networkInterface) {
+      if (i.family === 'IPv4' && !i.internal) {
+
+        IP = i.address
+        break
+      }
+    }
+
+    if (IP) return IP
+  }
+
+  return undefined
+}
+
