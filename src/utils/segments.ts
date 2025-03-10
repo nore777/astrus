7947cf848b460e40a78660c792a19c1f85b51ad6
lukas <lukas.noreikis@gmail.com>
2025-03-10 22:56:01 +0200

    improve url parser performance, and minor tweaks

--- src/utils/segments.ts
+++ src/utils/segments.ts
@@ -1,58 +1,45 @@
-import IDynamicSegment from '../types/IDynamicSegment'
 import ISegment from '../types/ISegment'
 
+// parses url passed from client into segments
 export function buildClientSegments(url: string): string[] {
-  if (!url) return []
-  let path = url
   let segments: string[] = []
   let seg = ''
   let flag = true
-  for (let i = 0; i <= path.length; i++) {
-    if (path[i] === '/' || i + 1 > path.length) {
+
+  for (let i = 0; i <= url.length; i++) {
+    if (url[i] === '/' || i + 1 > url.length) {
       if (flag) continue
       flag = true
       segments.push(seg)
       seg = ''
     } else {
       flag = false
-      seg += path[i]
+      seg += url[i]
     }
   }
+
   return segments
 }
 
-// parses server-side paths, and gives an object of segments
+// parses server-side route urls, and gives an object of segments
 export function buildServerSegments(url: string) {
-  if (!url) return []
-  let path = url
-
   let segments: ISegment[] = []
-  //let dynamicSegments: IDynamicSegment = {}
   let seg = ''
   let flag = true
-  for (let i = 0; i <= path.length; i++) {
-    if (path[i] === '/' || i + 1 > path.length) {
+
+  for (let i = 0; i <= url.length; i++) {
+    if (url[i] === '/' || i + 1 > url.length) {
       if (flag) continue
       if (seg.startsWith(':')) {
-        /*
-        seg = seg.slice(1, seg.length)
-        dynamicSegments = {
-          ...dynamicSegments,
-          [seg]: {
-            type: 'd',
-            value: '',
-          }
-        }
-        */
-        segments.push({ type: 'd', value: seg })
+        segments.push({ dynamic: true, value: seg.slice(1, seg.length) })
       } else {
-        segments.push({ type: 's', value: seg })
+        segments.push({ dynamic: false, value: seg })
       }
       flag = true
       seg = ''
     } else {
       flag = false
-      seg += path[i]
+      seg += url[i]
     }
   }
 
