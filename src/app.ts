b038037e59bec990db75b0c812ca4e9e502938a3
lukas <lukas.noreikis@gmail.com>
2025-03-10 22:47:39 +0200

    simplified

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,19 @@
-import http, { IncomingMessage, ServerResponse } from 'http'
+import http from 'http'
+import { Request, Response } from './class/exchange'
 import Routes from './class/routes'
-import { buildServerSegments, buildClientSegments } from './utils/segments';
+import testRoutes from './routes/testRoute'
 
 
 let routes = new Routes()
+routes.assign(testRoutes)
 
-routes.create('GET', '/hello-world', async () => {
-  try {
-    throw Error("XDD")
-  } catch (error) {
-    console.log(error)
-  }
+const server = http.createServer({
+  IncomingMessage: Request,
+  ServerResponse: Response
+}, async (req: Request, res: Response) => {
+
+
+  res.end()
 })
 
-
-let totalElapsedTime = 0;
-let requestCount = 0;
-
-http.createServer(function(req: IncomingMessage, res: ServerResponse) {
-  const start = process.hrtime();
-  ////////////////////////////////////////////////////////////////////////
-
-
-  const segments = buildClientSegments(req.url as string)
-  res.writeHead(200, "OK", { 'content-type': 'application/json' })
-  res.write(JSON.stringify({ segments }))
-  res.end();
-
-  ////////////////////////////////////////////////////////////////////////
-  const [seconds, nanoseconds] = process.hrtime(start);
-  totalElapsedTime += nanoseconds;
-  requestCount++;
-  const averageTime = totalElapsedTime / requestCount;
-  console.log(`Elapsed time: ${nanoseconds / 1000000}ms ${nanoseconds}ns`);
-  console.log(`Average     : ${averageTime}ns`);
-}).listen(8000, () => {
-  console.log('Server started successfully')
-});
+server.listen(8000, () => { console.log('Server started successfully') })
