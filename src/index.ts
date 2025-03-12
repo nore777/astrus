f7c1d9220be98e0309e58cd5e58d380884653e79
lukas <lukas.noreikis@gmail.com>
2025-03-12 23:12:42 +0200

    index exports, supports commonjs and module

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
-const jevel = "test"
+import { Jevel } from "./class/jevel";
+import { _REQ, Request } from "./class/request";
+import { _RES, Response } from "./class/response";
+import { ServerOptions } from "http";
 
-module.exports = jevel
+const options: ServerOptions = {
+  IncomingMessage: _REQ,
+  ServerResponse: _RES,
+}
+
+export default Jevel;
+export {
+  options,
+  Request,
+  Response,
+};
+
+module.exports = Jevel;
+Object.assign(module.exports, {
+  options,
+  Request,
+  Response,
+});
+
