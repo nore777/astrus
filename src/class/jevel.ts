27993087c334b3f951a54956052ea75e3fb0a0f9
lukas <lukas.noreikis@gmail.com>
2025-03-12 23:10:25 +0200

    add jevel class

---
+++ src/class/jevel.ts
@@ -0,0 +1,31 @@
+import Routes from './routes';
+import { _REQ, Request } from './request';
+import { _RES, Response } from './response';
+import { RequestListener } from 'node:http';
+import { THTTPRequestMethods } from '../types/types';
+
+export class Jevel {
+  constructor() {
+    this.#routes = new Routes()
+
+    this.server = (req: _REQ, res: _RES) => {
+      if (req !== undefined) {
+        const found = this.#routes.search(req) || null
+        if (found) {
+          return found(req.wrapper, res.wrapper)
+        }
+        res.writeHead(404, 'Not Found')
+        res.write(`Request [${req.method as string}] Not Found | ${req.url as string}`)
+        res.end()
+      }
+    }
+  }
+
+  #routes: Routes
+  server: RequestListener<typeof _REQ, typeof _RES> = (req: _REQ, res: _RES) => { }
+
+  route(method: THTTPRequestMethods, path: string, func: (req: Request, res: Response) => void) {
+    this.#routes.init(method, path, func)
+  }
+
+}
