d2002dca3af79fcaba24c02f54fd8329924c1b4e
lukas <lukas.noreikis@gmail.com>
2025-03-16 23:17:27 +0200

    multipart binary parser

--- src/class/astrus.ts
+++ src/class/astrus.ts
@@ -7,17 +7,87 @@ import IRoute from '../types/IRoute.js';
 
 
 const parseBody = (route: Function, req: _REQ, res: _RES): void => {
-  let body = ''
+  let body: Buffer = Buffer.from([])
 
   req.on('data', (chunk: any) => {
-    body += chunk
+    body = Buffer.concat([body, chunk])
   });
 
   req.on('end', () => {
+    const contentType = req.headers['content-type']
+
     try {
-      if (req.headers['content-type'] === 'application/json') {
-        req.wrapper.body = JSON.parse(body)
+      if (contentType === 'application/json') {
+        // parse json
+        req.wrapper.body = JSON.parse(body.toString())
+
+      } else if (contentType?.startsWith('multipart/form-data')) {
+        // parse multipart
+        const boundary = '--' + contentType.split('boundary=')[1]
+        let lastBuffer: any = false
+        let lastLine = ''
+        let lastEndOfLine = 0
+        let prevEndOfLine = 0
+        let parsingValue = false
+        let value: Buffer = Buffer.from([])
+        let parsedBody: any = {}
+        let mime = ''
+        let tmp: any = {}
+
+        for (let i = 0; i < body.length; i++) {
+
+          if (body[i] === 0x0d && body[i + 1] === 0x0a) {
+            lastBuffer = body.subarray(lastEndOfLine, i)
+            lastLine = lastBuffer.toString()
+
+            prevEndOfLine = lastEndOfLine
+            lastEndOfLine = i
+            const _lastLine = lastLine.trim().toLowerCase()
+            if (_lastLine.startsWith('content-disposition')) {
+              const _tmp = lastLine.trim().split(';')
+              for (let j = 1; j < _tmp.length; j++) {
+                const keyVal = _tmp[j].trim().split('=')
+                tmp = {
+                  ...tmp,
+                  [keyVal[0]]: keyVal[1].substring(1, keyVal[1].length - 1)
+                }
+              }
+
+            } else if (_lastLine.startsWith('content-type')) {
+              mime = lastLine.split(':')[1].trim()
+
+            } else if (_lastLine === '') {
+              parsingValue = true
+
+            } else if (parsingValue && _lastLine !== boundary && _lastLine !== boundary + '--') {
+              value = Buffer.concat([value, lastBuffer])
+
+            } else if (parsingValue && (_lastLine === boundary || _lastLine === boundary + '--')) {
+              if (tmp.filename) {
+                parsedBody = {
+                  ...parsedBody,
+                  [tmp.name]: {
+                    mime: mime,
+                    filename: tmp.filename,
+                    value: value.subarray(2, value.length),
+                  }
+                }
+                tmp.filename = undefined
+              } else {
+                parsedBody = {
+                  ...parsedBody,
+                  [tmp.name]: value.subarray(2, value.length).toString()
+                }
+              }
+              mime = ''
+              value = Buffer.from([])
+            }
+          }
+        }
+        req.wrapper.body = parsedBody
+
       } else {
+        // plain text
         req.wrapper.body = body
       }
       route(req.wrapper, res.wrapper)
@@ -36,7 +106,7 @@ const continueToRoute: { [key in any]: (route: Function, req: _REQ, res: _RES) =
   }
 }
 
-export default class Jevel {
+export default class Astrus {
   private routes: Routes
   private requestListener: RequestListener<typeof _REQ, typeof _RES> = (req: _REQ, res: _RES) => { }
 
@@ -48,8 +118,7 @@ export default class Jevel {
         try {
           continueToRoute[req.method as THTTPRequestMethods](found, req, res)
         } catch (error) {
-          console.log(error)
-          res.write('Not allowed')
+          res.write('Method not allowed')
           res.end()
         }
       } else {
@@ -72,19 +141,17 @@ export default class Jevel {
       this.routes.init({ method: methodRoute, path, func })
     } else {
       if (Array.isArray(methodRoute)) {
-        console.log("Initializing array of routes")
         for (let i = 0; i < methodRoute.length; i++) {
           this.routes.init(methodRoute[i] as IRoute)
         }
       } else {
-        console.log(methodRoute, "Initializing single route")
         this.routes.init(methodRoute as IRoute)
       }
     }
   }
 
-  start(port: number) {
+  start(port: number, callback: () => void) {
     const server = http.createServer({ IncomingMessage: _REQ, ServerResponse: _RES }, this.requestListener)
-    server.listen(port, () => { console.log('[Server Started]') })
+    server.listen(port, callback)
   }
 }
