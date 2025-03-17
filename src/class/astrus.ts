44d03ff69e3cb78cad79850b08ece638b289ced4
lukas <lukas.noreikis@gmail.com>
2025-03-18 00:07:27 +0200

    static method to serve static files

--- src/class/astrus.ts
+++ src/class/astrus.ts
@@ -4,98 +4,9 @@ import { _RES, Response } from './response.js';
 import http, { RequestListener } from 'node:http';
 import { THTTPRequestMethods } from '../types/types.js';
 import IRoute from '../types/IRoute.js';
+import parseBody from '../utils/parseBody.js';
+import fs from 'fs'
 
-
-const parseBody = (route: Function, req: _REQ, res: _RES): void => {
-  let body: Buffer = Buffer.from([])
-
-  req.on('data', (chunk: any) => {
-    body = Buffer.concat([body, chunk])
-  });
-
-  req.on('end', () => {
-    const contentType = req.headers['content-type']
-
-    try {
-      if (contentType === 'application/json') {
-        // parse json
-        req.wrapper.body = JSON.parse(body.toString())
-
-      } else if (contentType?.startsWith('multipart/form-data')) {
-        // parse multipart
-        const boundary = '--' + contentType.split('boundary=')[1]
-        let lastBuffer: any = false
-        let lastLine = ''
-        let lastEndOfLine = 0
-        let prevEndOfLine = 0
-        let parsingValue = false
-        let value: Buffer = Buffer.from([])
-        let parsedBody: any = {}
-        let mime = ''
-        let tmp: any = {}
-
-        for (let i = 0; i < body.length; i++) {
-
-          if (body[i] === 0x0d && body[i + 1] === 0x0a) {
-            lastBuffer = body.subarray(lastEndOfLine, i)
-            lastLine = lastBuffer.toString()
-
-            prevEndOfLine = lastEndOfLine
-            lastEndOfLine = i
-            const _lastLine = lastLine.trim().toLowerCase()
-            if (_lastLine.startsWith('content-disposition')) {
-              const _tmp = lastLine.trim().split(';')
-              for (let j = 1; j < _tmp.length; j++) {
-                const keyVal = _tmp[j].trim().split('=')
-                tmp = {
-                  ...tmp,
-                  [keyVal[0]]: keyVal[1].substring(1, keyVal[1].length - 1)
-                }
-              }
-
-            } else if (_lastLine.startsWith('content-type')) {
-              mime = lastLine.split(':')[1].trim()
-
-            } else if (_lastLine === '') {
-              parsingValue = true
-
-            } else if (parsingValue && _lastLine !== boundary && _lastLine !== boundary + '--') {
-              value = Buffer.concat([value, lastBuffer])
-
-            } else if (parsingValue && (_lastLine === boundary || _lastLine === boundary + '--')) {
-              if (tmp.filename) {
-                parsedBody = {
-                  ...parsedBody,
-                  [tmp.name]: {
-                    mime: mime,
-                    filename: tmp.filename,
-                    value: value.subarray(2, value.length),
-                  }
-                }
-                tmp.filename = undefined
-              } else {
-                parsedBody = {
-                  ...parsedBody,
-                  [tmp.name]: value.subarray(2, value.length).toString()
-                }
-              }
-              mime = ''
-              value = Buffer.from([])
-            }
-          }
-        }
-        req.wrapper.body = parsedBody
-
-      } else {
-        // plain text
-        req.wrapper.body = body
-      }
-      route(req.wrapper, res.wrapper)
-    } catch (error) {
-      res.wrapper.error(400, "Bad Request")
-    }
-  })
-}
 
 const continueToRoute: { [key in any]: (route: Function, req: _REQ, res: _RES) => void | boolean } = {
   'GET': (route, req, res) => {
@@ -108,6 +19,7 @@ const continueToRoute: { [key in any]: (route: Function, req: _REQ, res: _RES) =
 
 export default class Astrus {
   private routes: Routes
+  private statics = {}
   private requestListener: RequestListener<typeof _REQ, typeof _RES> = (req: _REQ, res: _RES) => { }
 
   constructor() {
@@ -127,6 +39,18 @@ export default class Astrus {
         res.end()
       }
     }
+  }
+
+  static(directory: string, url: string) {
+    this.route('GET', url + '/*', async (req, res) => {
+      try {
+        const filePath = directory + req.url.substring(url.length, req.url.length)
+        res.send(fs.readFileSync(filePath))
+
+      } catch (error) {
+        res.error()
+      }
+    })
   }
 
   route(method: THTTPRequestMethods, path: string, func: (req: Request, res: Response) => void): void;
@@ -155,3 +79,4 @@ export default class Astrus {
     server.listen(port, callback)
   }
 }
+
