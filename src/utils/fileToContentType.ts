b43e092540f8252527ab5d63d082f4f1415527d9
lukas <lukas.noreikis@gmail.com>
2025-03-18 16:23:04 +0200

    map file extentions to content type

---
+++ src/utils/fileToContentType.ts
@@ -0,0 +1,14 @@
+const fileToContentType: any = {
+  'txt': 'text/plain',
+  'html': 'text/html',
+  'css': 'text/css',
+  'js': 'text/javascript',
+
+  'png': 'image/png',
+  'jpeg': 'image/jpeg',
+  'jpg': 'image/jpeg',
+  'webp': 'image/webp',
+  'svg': 'image/svg+xml',
+}
+
+export default fileToContentType
