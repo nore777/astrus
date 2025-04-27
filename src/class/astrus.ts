904e46847b2c81b497971aba4f3381bb61717629
lukas <lukas.noreikis@gmail.com>
2025-04-27 12:51:25 +0300

    https support

--- src/class/astrus.ts
+++ src/class/astrus.ts
@@ -2,49 +2,52 @@ import Routes from './routes.js';
 import { _REQ, Request } from './request.js';
 import { _RES, Response } from './response.js';
 import http, { RequestListener } from 'node:http';
+import https from 'node:https'
+import { ServerOptions as httpOptions } from 'node:http';
+import { ServerOptions as httpsOptions } from 'node:https';
 import { THTTPRequestMethods } from '../types/types.js';
 import continueToRoute from '../utils/continueToRoute.js';
 import ICorsOptions from '../types/ICorsOptions.js';
+import { WithMethods } from '../types/types.js';
 
 import route from '../functions/route.js';
 import serveStatic from '../functions/serveStatic.js';
-import { corsDefaultOptions, corsOptions } from '../functions/cors.js';
+import { corsDefaultOptions, corsOptions, handleCors } from '../functions/cors.js';
 
 export default class Astrus {
   route: typeof route
   corsOptions: typeof corsOptions
   serveStatic: typeof serveStatic
+  protected handleCors: typeof handleCors
   protected routes: Routes
   protected cors: ICorsOptions
   private requestListener: RequestListener<typeof _REQ, typeof _RES> = (_req: _REQ, _res: _RES) => { }
+
 
   constructor() {
     this.routes = new Routes()
     this.route = route.bind(this)
     this.cors = corsDefaultOptions
     this.corsOptions = corsOptions.bind(this)
+    this.handleCors = handleCors.bind(this)
     this.serveStatic = serveStatic.bind(this)
+
 
     this.requestListener = (req: _REQ, res: _RES) => {
 
-      // cors placeholder
-      if (req.method === 'OPTIONS' && this.cors) {
-        res.setHeader('Access-Control-Allow-Methods', this.cors.methods!.join(', '));
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-        res.setHeader('Access-Control-Allow-Credentials', 'true');
-        res.setHeader('Access-Control-Max-Age', '1');
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        return res.end();
-      } else {
-        if (req.headers.origin && this.cors.origin!.includes(req.headers.origin)) {
-          res.setHeader('Access-Control-Allow-Origin', '*');
-        }
+      // cors
+      // TODO: Create global middlewares that run on every route
+      // and add cors to that instead
+      if (this.handleCors(req, res)) {
+        res.end()
       }
 
+      // search for route
       const found = this.routes.search(req) || null
 
       if (found) {
         try {
+          // middlewares
           if (found.middlewares) {
             for (let middleware of found.middlewares) {
               if (!res.writableEnded) {
@@ -52,13 +55,13 @@ export default class Astrus {
               } else break
             }
           }
-
+          // continue to route
           if (!res.writableEnded) {
             continueToRoute[req.method as THTTPRequestMethods](found.func, req, res)
           }
-
         } catch (error) {
-          res.write(error)
+          console.error(error)
+          res.writeHead(500, 'Server Error')
           res.end()
         }
       } else {
@@ -69,9 +72,27 @@ export default class Astrus {
     }
   }
 
-  start(port: number, callback: () => void) {
-    const server = http.createServer({ IncomingMessage: _REQ, ServerResponse: _RES }, this.requestListener)
-    server.listen(port, callback)
+  start(protocol: 'http' | 'https', port: number, options: httpOptions | httpsOptions, callback: () => void) {
+    if (protocol === 'http') {
+      const server = http.createServer({
+        ...options,
+        IncomingMessage: _REQ,
+        ServerResponse: _RES,
+      },
+        this.requestListener
+      )
+      server.listen(port, callback)
+
+    } else if (protocol === 'https') {
+      const server = https.createServer({
+        ...options,
+        IncomingMessage: _REQ,
+        ServerResponse: _RES,
+      },
+        this.requestListener
+      )
+      server.listen(port, callback)
+    }
   }
 }
 
